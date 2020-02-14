f301db295a545b83caeb57bb3930dc57f10a78ac
Jinki Jung <your.jinki.jung@gmail.com>
2020-02-14 13:02:56 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,10 +41,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
-
-master_doc = 'index'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
@@ -55,3 +52,5 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+master_doc = 'index'
